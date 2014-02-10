383ab3d72c1a7a0c3c48fb1a54c59ccdb4bb46f0
mrzl <lzrm@mrzl.net>
2014-02-10 16:46:14 +0100

    moving some code to Util classs
    Signed-off-by: mrzl <lzrm@mrzl.net>

--- src/main/Util.java
+++ src/main/Util.java
@@ -4,6 +4,7 @@ import processing.core.PApplet;
 import processing.core.PConstants;
 import processing.core.PVector;
 import toxi.geom.Vec3D;
+import toxi.geom.mesh.LaplacianSmooth;
 import toxi.geom.mesh.WETriangleMesh;
 import toxi.volume.*;
 
@@ -16,44 +17,7 @@ import java.util.Calendar;
  * Project: MeshBrushDrawer
  */
 public class Util {
-    public static void drawMeshCircle( VolumetricSpace volume, IsoSurface surface, VolumetricBrush brush, float radius, float numPoints, WETriangleMesh mesh, float thickness ) {
-        float angle = PConstants.TWO_PI / numPoints;
-        for ( int i = 0; i < numPoints; i++ ) {
-            PVector start = new PVector( radius * PApplet.sin( angle * i ), radius * PApplet.cos( angle * i ), 0 );
-            PVector end = new PVector( radius * PApplet.sin( angle * ( i + 1 ) ), radius * PApplet.cos( angle * ( i + 1 ) ), 0 );
-            connectPoints( brush, start, end, thickness );
-        }
-    }
 
-    public static void drawRandomGrowRing( PApplet parent, VolumetricSpace volume, IsoSurface surface, VolumetricBrush brush, float radius, float numPoints, WETriangleMesh mesh, float thickness ) {
-        float angle = PConstants.TWO_PI / numPoints;
-        int randomPoint = ( int ) parent.random( numPoints );
-        for ( int height = 0; height < 500; height++ ) {
-            randomPoint += ( int ) parent.random( -5, 5 );
-
-            PVector start = new PVector( radius * PApplet.sin( angle * randomPoint ), radius * PApplet.cos( angle * randomPoint ), -height );
-            PVector end = new PVector( radius * PApplet.sin( angle * ( randomPoint + 1 ) ), radius * PApplet.cos( angle * ( randomPoint + 1 ) ), -height + 1 );
-            connectPoints( brush, start, end, thickness );
-        }
-
-        volume.closeSides();
-        surface.reset();
-        surface.computeSurfaceMesh( mesh, .1f );
-        mesh.faceOutwards();
-        mesh.removeUnusedVertices();
-        mesh.rebuildIndex();
-        mesh.computeFaceNormals();
-        mesh.computeVertexNormals();
-    }
-
-    public static void connectPoints( VolumetricBrush brush, PVector start, PVector end, float thickness ) {
-        for ( float i = 0.0f; i <= 1; i += 0.1 ) {
-            float lerpX = PApplet.lerp( start.x, end.x, i );
-            float lerpY = PApplet.lerp( start.y, end.y, i );
-            float lerpZ = PApplet.lerp( start.z, end.z, i );
-            brush.drawAtAbsolutePos( new Vec3D( lerpX, lerpY, lerpZ ), thickness );
-        }
-    }
 
     public static WETriangleMesh meshVoxelizer( WETriangleMesh mesh, int meshVoxRes,
                                                 int wallThickness, float surfaceIso ) {
@@ -73,5 +37,10 @@ public class Util {
     public static String getTimeStamp() {
         Calendar now = Calendar.getInstance();
         return String.format( "%1$ty%1$tm%1$td_%1$tH%1$tM%1$tS", now );
+    }
+
+
+    public static void smoothMesh( WETriangleMesh mesh, int amount ) {
+        new LaplacianSmooth().filter( mesh, amount );
     }
 }
